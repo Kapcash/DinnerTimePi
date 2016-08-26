1d0cdfd760296aa447bed54a79d6c67b238d9dd7
Kapcash <florent.ct@orange.fr>
2016-08-26 20:54:18 +0200

    Added new MVC files

---
+++ src/client/MainClient.java
@@ -0,0 +1,19 @@
+package client;
+ 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainClient {
+
+	public static void main(String[] args) {
+
+		String host = "192.168.1.35";
+		int port = 35150;
+
+/*		Thread client1 = new Thread(new ClientConnexion(host, port));
+		client1.start();
+*/
+		new ClientConnexion(host,port); //For notification test
+	}
+}
---
+++ src/client/MainListener.java
@@ -0,0 +1,46 @@
+package client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MainListener extends MouseAdapter implements ActionListener{
+	
+	MainView view;
+	ClientConnexion client;
+
+	public MainListener(MainView v, ClientConnexion c){
+		this.view = v;
+		this.client = c;
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		/*if(src == ok){
+			client.send("OK");
+			v.hideNotification();
+		}
+		else if(src == min){
+			client.send("2MIN");
+			v.hideNotification();
+		}
+		else if(src == no){
+			client.send("NO");
+			v.hideNotification();
+		}*/
+		if(src == view.getTrayIcon()){
+			view.displayNotification();
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e){
+		Object src = e.getSource();
+
+		view.hideGUI();
+	}
+
+}
